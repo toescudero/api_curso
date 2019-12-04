18af9e7204113d1741210fc666e4fcc121f181dd
tomas escudero <t.e.tomasescudero@gmail.com>
2019-12-04 15:58:03 -0300

    correct rubocop

--- app/workers/checking_dates.rb
+++ app/workers/checking_dates.rb
@@ -1,11 +1,13 @@
+# frozen_string_literal: true
+
 class CheckingDates
-    def perform
-        queue_challenges = RanksChallenge.where(status: true)
-        queue_challenges.each do |r|
-            if r.expire_date < Time.now
-                r.status = false
-                r.save
-            end
+  def perform
+    queue_challenges = RanksChallenge.where(status: true)
+    queue_challenges.each do |r|
+        if r.expire_date < Time.now
+          r.status = false
+          r.save
         end
     end
   end
+end
--- lib/tasks/check_expired.rake
+++ lib/tasks/check_expired.rake
@@ -1,7 +1,9 @@
-desc "check the expiration dates of the challenges"
-##FUTURE FEATURE ADD WEB MAILER
-task :check_expired => :environment do
-  puts "checking dates"
+# frozen_string_literal: true
+
+desc 'check the expiration dates of the challenges'
+# #FUTURE FEATURE ADD WEB MAILER
+task check_expired: :environment do
+  puts 'checking dates'
   CheckingDates.new.perform
-  puts "done."
+  puts 'done.'
 end
